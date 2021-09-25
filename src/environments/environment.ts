6a26a70ec1c6cdd0d2228ec922cde435bc660534
LakshanRM <lakshanM@fortude.co>
2021-09-25 07:41:50 +0530

    Upload url Change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   STUDENTS_URL: 'http://localhost:4200/assets/data/students.json',
-  UPLOAD_URL: 'http://localhost:3000/uploads/uploadFile',
+  UPLOAD_URL: 'http://localhost:3001/uploads/uploadFile',
   GRAPHQL_URL: 'http://localhost:3000/graphql',
   KENDO_UI_LICENSE:
     'eyJhbGciOiJSUzI1NiIsInR5cCI6IkxJQyJ9.eyJwcm9kdWN0cyI6W3sidHJpYWwiOnRydWUsImNvZGUiOiJLRU5ET1VJQU5HVUxBUiIsImxpY2Vuc2VFeHBpcmF0aW9uRGF0ZSI6MTYzNDQzNzAxMn1dLCJpbnRlZ3JpdHkiOiJveERsXC9qdEt5eUwxaXdabG9iOXBITFBiRWxVPSIsImxpY2Vuc2VIb2xkZXIiOiJsYWtzaGFubUBmb3J0dWRlLmNvIiwiaWF0IjoxNjMxOTMwMDUzLCJhdWQiOiJsYWtzaGFubUBmb3J0dWRlLmNvIn0.FH4a_KVLDHRUxm8syVm6dxZO95Er1dZK8iht64WBojNvElMZtNh_bcIkGX9QlbkfRFHHY_Fe_rCSx9QBW-KhvpqiNQljWiiPH_ZLANRkWEkMo47SHYyEG2g1uQrWsYQonf94GrdfEf9QtoxRxg3WQl-A0lewjWSB7dnIIessS65UZODmvb2nmtoRtuKpcEKxam0URoLMZnRqzYLwAMjql_pz2Yaw2icut7rZVSzXDbFl9qc1UcD4F5gp81zSgWVXCFiG73IQnjNI0ucD3zr4f8pzrlcRidM8Z5fr6Xg7uiv2EAsmK4WBM5GXwmB9EScfts7BFIaBwOal28qqM1oD4g',
